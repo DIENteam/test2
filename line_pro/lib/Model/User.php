f72541a92905918e4fb24f3724eb1600ef468550
takkuhiro <n.hirotaka26@gmail.com>
2017-07-17 12:50:13 +0900

    Add files via upload

---
+++ line_pro/lib/Model/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace MyApp\Model;
+
+class User extends \MyApp\Model {
+
+  public function create($values) {
+    $stmt = $this->db->prepare("insert into users (email, password, created, modified) values (:email, :password, now(), now())");
+    $res = $stmt->execute([
+      ':email' => $values['email'],
+      ':password' => password_hash($values['password'], PASSWORD_DEFAULT)
+    ]);
+    if ($res === false) {
+      throw new \MyApp\Exception\DuplicateEmail();
+    }
+  }
+
+  //created by myself_start
+  public function create_sentence($values) {
+    $stmt = $this->db->prepare("insert into adv (u_id, title, body, ad_time) values (1, :title, :body, now())");
+    $res = $stmt->execute([
+      ':title' => $values['title'],
+      ':body' => $values['body']
+    ]);
+    if ($res === false) {
+      throw new \MyApp\Exception\InvalidSentence();
+    }
+  }
+
+  public function flag_update_accept($num) {
+    $stmt = $this->db->prepare("update adv set flag = 1 where adv_id = $num");
+    //if()
+    $res = $stmt->execute();
+    if ($res === false) {
+      //throw new \MyApp\Exception\InvalidSentence();
+    }
+  }
+
+  public function flag_update_reject($num) {
+    $stmt = $this->db->prepare("update adv set flag = 0 where adv_id = $num");
+    $res = $stmt->execute();
+    //ここで下のif文を通ってしまっている
+    if ($res === false) {
+      throw new \MyApp\Exception\InvalidInsertFlag();
+    }
+  }
+
+  // public function check_sentence($values) {
+  //   $stmt = $this->db->prepare("select adv_id, u_id, title, body, ad_time, flag from adv order by adv_id desc");
+  //   $stmt->execute([
+  //
+  //     ':adv_id' => $values['adv_id'],
+  //     ':u_id' => $values['u_id'],
+  //     ':title' => $values['title'],
+  //     ':body' => $values['body'],
+  //     ':ad_time' => $values['ad_time'],
+  //     ':flag' => $values['flag']
+  //   ]);
+  // }
+  //created by myself_stop
+
+  public function login($values) {
+    $stmt = $this->db->prepare("select * from users where email = :email");
+    $stmt->execute([
+      ':email' => $values['email']
+    ]);
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+    $user = $stmt->fetch();
+
+    if (empty($user)) {
+      throw new \MyApp\Exception\UnmatchEmailOrPassword();
+    }
+
+    if (!password_verify($values['password'], $user->password)) {
+      throw new \MyApp\Exception\UnmatchEmailOrPassword();
+    }
+
+    return $user;
+  }
+
+  public function findAll() {
+    $stmt = $this->db->query("select * from users order by id");
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+    return $stmt->fetchAll();
+  }
+
+  public function findAll_sentence() {
+    $stmt = $this->db->query("select * from adv");
+    $stmt->setFetchMode(\PDO::FETCH_CLASS, 'stdClass');
+    return $stmt->fetchAll();
+  }
+}
