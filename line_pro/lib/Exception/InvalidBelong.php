d28cc08f1f39556bfbe4a09b7ff826c5e81b4352
kwashima <k-kawashima@atfreaks.com>
2017-07-17 12:48:20 +0900

    delete
    delete

--- line_pro/lib/Exception/InvalidBelong.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MyApp\Exception;
-
-class InvalidBelong extends \Exception {
-  protected $message = 'Invalid Belong!';
-}
--- line_pro/lib/Exception/InvalidSentence.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MyApp\Exception;
-
-class InvalidSentence extends \Exception {
-  protected $message = 'Invalid Sentence!';
-}
